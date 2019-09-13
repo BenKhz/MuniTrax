9d3023735965ad509a88487094ca3946da44d2b2
Ben Bernardy <benbernard@gmail.com>
2019-09-12 18:20:43 -0700

    PARSER ERROR HANDLING EXCEPTIONS

--- XML_app.py
+++ XML_app.py
@@ -55,46 +55,49 @@ def parse_xml():    # working generator function.
 
         [<route number>,<direction>,<Stop location>,<nested list of departs>]
     '''
-
-    tree = ET.parse('TempXML.xml')
-    root = tree.getroot()
-    #    table_data = []
-    for i in root.findall("./predictions"):
-        try:
-            for j in i.findall('./direction'):
-                predict_list = j.findall('./prediction')
-                min_list = []
-                min_count = 0
-                for elems in predict_list:
-                    if min_count < 2: # Stops appending at 2 entries
-                        min_list.append(elems.get('minutes'))
-                        min_count += 1
-                    elif len(min_list) < 2:
-                        min_list.append('Min')
+    try:
+        tree = ET.parse('TempXML.xml')
+        root = tree.getroot()
+        #    table_data = []
+        for i in root.findall("./predictions"):
+            try:
+                for j in i.findall('./direction'):
+                    predict_list = j.findall('./prediction')
+                    min_list = []
+                    min_count = 0
+                    for elems in predict_list:
+                        if min_count < 2: # Stops appending at 2 entries
+                            min_list.append(elems.get('minutes'))
+                            min_count += 1
+                        elif len(min_list) < 2:
+                            min_list.append('Min')
+                        else:
+                            min_list.append('Min')
+                            break
+                    row_data = []
+                    route = i.get('routeTag')
+                    stop = i.get('stopTitle')
+                    direct = j.get('title')
+                    if "Presidio" in stop and "Presidio" in direct:
+                        if "Park" in direct:
+                            row_data.append(route)
+                            row_data.append(direct)
+                            row_data.append(stop)
+                            row_data.append(min_list)
+                            yield (row_data)
+                        else:
+                            continue
                     else:
-                        min_list.append('Min')
-                        break
-                row_data = []
-                route = i.get('routeTag')
-                stop = i.get('stopTitle')
-                direct = j.get('title')
-                if "Presidio" in stop and "Presidio" in direct:
-                    if "Park" in direct:
                         row_data.append(route)
                         row_data.append(direct)
                         row_data.append(stop)
                         row_data.append(min_list)
                         yield (row_data)
-                    else:
-                        continue
-                else:
-                    row_data.append(route)
-                    row_data.append(direct)
-                    row_data.append(stop)
-                    row_data.append(min_list)
-                    yield (row_data)
-        except IndexError:
-            print("No 'direction' tag in " + i.get('routeTitle'))
+            except IndexError:
+                print("No 'direction' tag in " + i.get('routeTitle'))
+    except ET.ParseError:
+        print("ET.ParseError occurred at : " + time.asctime())
+        time.sleep(30)
 
 
 def populate_table():
